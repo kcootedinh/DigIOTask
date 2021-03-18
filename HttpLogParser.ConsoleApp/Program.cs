d0de933f72c4688ea8afc58870e093d66567f83c
Kieran Coote-Dinh <kcoote@users.noreply.github.com>
2021-03-18 23:27:01 +1100

    Connect up command line arguments to parser

--- HttpLogParser.ConsoleApp/Program.cs
+++ HttpLogParser.ConsoleApp/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.IO;
+using HttpLogParser.ConsoleApp;
 
-namespace HttpLogParser.ConsoleApp
+if (args.Length == 0)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    Console.WriteLine("Please specify an absolute path to a log file");
+    return;
 }
+
+var logData = File.ReadAllText(args[0]);
+var analytics = HttpLogAnalytics.Parse(logData);
+
+Console.WriteLine($"Unique IP addresses: {analytics.UniqueIps} ");
+Console.WriteLine($"Most active IP addresses: {Environment.NewLine}" +
+                  $"* {analytics.MostActiveIps[0]} {Environment.NewLine}" +
+                  $"* {analytics.MostActiveIps[1]} {Environment.NewLine}" +
+                  $"* {analytics.MostActiveIps[2]} {Environment.NewLine}");
+Console.WriteLine($"Most visited URLs: {Environment.NewLine}" +
+                  $"* {analytics.MostVisitedUrls[0]} {Environment.NewLine}" +
+                  $"* {analytics.MostVisitedUrls[1]} {Environment.NewLine}" +
+                  $"* {analytics.MostVisitedUrls[2]} {Environment.NewLine}");
